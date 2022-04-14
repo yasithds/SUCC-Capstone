eb6fca2c8df1154365423f23920678defd958a71
yasithds <56009673+yasithds@users.noreply.github.com>
2022-04-14 16:38:47 -0400

    Driver change
    L293D motor driver shield added with NEMA 17 steppers

--- EncoderTest/EncoderTest.ino
+++ EncoderTest/EncoderTest.ino
@@ -3,11 +3,16 @@
 // AF_Stepper motor2(2038,2);
 
 //Rotary Encoder Inputs
+#include <SimpleKeypad.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>   
 
 #define inputCLK 10
 #define inputDT 11
 
-#include <SimpleKeypad.h>
+#define SLAVE_ADDR 9
+
+LiquidCrystal_I2C lcd(0x27,20,4);     
 
 const byte nb_rows = 4;                         // four rows
 const byte nb_cols = 4;                         // four columns
@@ -32,25 +37,22 @@ int previousStateCLK;
 String encdir = "";
 
 void setup() {
-
+  Wire.begin(); //starts i2c as master
+  
   // Set encoder pins as inputs
   pinMode (inputCLK, INPUT);
   pinMode (inputDT, INPUT);
 
-  // pinMode (2,OUTPUT);
-  // pinMode (13,OUTPUT);
-  // pinMode (9,OUTPUT);
-  //     pinMode (10,OUTPUT);
-
-  //   digitalWrite(2, HIGH);
-  //  digitalWrite(13, HIGH);
-  //   digitalWrite(9, HIGH);
-  //  digitalWrite(10, HIGH);
-
-
-  // Set LED pins as outputs
-  //  pinMode (ledCW,OUTPUT);
-  // pinMode (ledCCW,OUTPUT);
+  lcd.init();
+  lcd.backlight();                
+  lcd.setCursor(0,0);
+  lcd.print("Hello!");
+  lcd.setCursor(0,1);
+  lcd.print("Funny!");
+  lcd.setCursor(3,2);            
+  lcd.print("Enjoy!");
+  lcd.setCursor(8,3);             
+  lcd.print("Arduino!");      
 
   // Setup Serial Monitor
   Serial.begin (9600);
@@ -59,8 +61,6 @@ void setup() {
   // Assign to previousStateCLK variable
   previousStateCLK = digitalRead(inputCLK);
 
-  // motor1.setSpeed(100);
-  //   motor2.setSpeed(100);
 }
 
 void loop() {
@@ -76,8 +76,6 @@ void loop() {
     if (digitalRead(inputDT) != currentStateCLK) {
       counter --;
       encdir = "CCW";
-      //  digitalWrite(ledCW, LOW);
-      // digitalWrite(ledCCW, HIGH);
 
     } else {
       // Encoder is rotating clockwise
@@ -93,15 +91,37 @@ void loop() {
     Serial.println(counter);
   }
 
-  //FORWARD/BACKWARD, SINGLE/DOUBLE/INTERLEAVE/MICROSTEP
-  // Serial.println("Single coil steps");
-  //motor1.step(2048, FORWARD, SINGLE);
-  //motor2.step(2048, FORWARD, SINGLE);
-
    char key = kp1.getKey();                      // The getKey function scans the keypad every 10 ms and returns a key only one time, when you start pressing it
+  if (key) {                                    // If getKey returned any key
+    Serial.println(key);                        // it is printed on the serial monitor
+    lcd.print(key);   
+  }
   if (key=='A') {                                    // If getKey returned any key
     Serial.println(key);                        // it is printed on the serial monitor
-  }
+    Wire.beginTransmission(SLAVE_ADDR);
+    Wire.write(45);
+    Wire.write(1);
+    Wire.endTransmission();
+    
+  } else if (key=='B') {
+     Serial.println(key);                        // it is printed on the serial monitor
+    Wire.beginTransmission(SLAVE_ADDR);
+    Wire.write(90);
+    Wire.write(1);
+    Wire.endTransmission();
+    
+  } else if (key=='C') {
+    Wire.beginTransmission(SLAVE_ADDR);
+    Wire.write(counter);
+    Wire.write(2);
+    Wire.endTransmission();
+    
+  } else if (key=='D') {
+    Wire.beginTransmission(SLAVE_ADDR);
+    Wire.write(0);
+    Wire.write(4);
+    Wire.endTransmission();
+  } 
 
   // Update previousStateCLK with the current state
   previousStateCLK = currentStateCLK;
--- StepperTest/StepperTest.ino
+++ StepperTest/StepperTest.ino
@@ -1,9 +1,101 @@
+#include <Wire.h>
+#include <AFMotor.h>
+
+#define SLAVE_ADDR 9
+
+AF_Stepper motor1(200,1); //#steps per rev, motor placement on board
+AF_Stepper motor2(200,2);
+
+String data ="";
+int angle =0;
+int stepNum;
+int tilt = 0;
+int ratio=1;// gear ratio (40)
+int indexAngle=0;
+
 void setup() {
   // put your setup code here, to run once:
+Serial.begin (9600);
+Wire.begin(SLAVE_ADDR); //I2C as slave
+
+Wire.onReceive(receiveEvent);
+Wire.onRequest(requestEvent);
+
+motor1.setSpeed(100);
+motor2.setSpeed(100);
+}
+
+void requestEvent(){
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void receiveEvent(int howMany){
+  data ="";
+  int c =0;
+  while(1 < Wire.available()){
+    c = Wire.read();          
+    Serial.print(c);             
+    //data += c;//used to determine the angle (in degrees)
+  }
+
+  int action = Wire.read(); // determines the actual motor to manipulate
+  Serial.println(action);
+
+  //angle = data.toInt();
+  
+  if (action == 1){ //motor 1 = tilt
+    stepNum = map(c, -90, 90, (-200/4)*ratio, (200/4)*ratio);
+    if (stepNum >= tilt && stepNum+tilt <= ratio){ //50* (1:40) from gearing
+      
+    Serial.print("angle: ");
+    Serial.println(angle);
+    Serial.print("steps: ");
+    Serial.println(stepNum);
+    motor1.step(stepNum-tilt, FORWARD, SINGLE);
+    tilt = stepNum - tilt;
+    
+    } else if (stepNum >= tilt && stepNum+tilt >= ratio){
+      motor1.step(ratio - tilt, FORWARD, SINGLE);
+      tilt = ratio;
+      
+    } else if (stepNum>0 && stepNum < tilt && stepNum+tilt <= -ratio){
+      motor1.step(stepNum, BACKWARD, SINGLE);
+      tilt = tilt - stepNum;  
+    } else if (stepNum < tilt && stepNum+tilt <= -ratio){
+      motor1.step(tilt, BACKWARD, SINGLE);
+      tilt = 0; 
+    }
+
+      
+    motor1.step(stepNum+tilt, BACKWARD, SINGLE);
+    tilt += stepNum;
+    
+    Serial.print("Current Tilt: ");
+    Serial.println(tilt);
+    
+  } else if (action==2){ // motor 2 = indexing angle
+    // Set indexing angle
+    stepNum = map(c, 0, 180, 0, (200/2)*ratio);
+  } else if (action==3){ // motor 2 - indexing divisions
+    // Set indexing by divisions
+    stepNum = (200/c) * ratio;
+  } else {
+    // Continue with current index
+     motor2.step(stepNum, FORWARD, SINGLE);
+  }
+} 
+
+
+
+void loop() {
+  // stuff here is basically just reference material -------------------------
+  // put your main code here, to run repeatedly:
+  //FORWARD/BACKWARD, SINGLE/DOUBLE/INTERLEAVE/MICROSTEP
+// Serial.println("Single coil steps");
+  //motor1.step(200, FORWARD, SINGLE);
+  delay(200);
+  // motor2.step(200, FORWARD, SINGLE);
+  //Serial.println(data);
+  delay(200);
 }
